cc6babf777a28ccb53e8bff3286f4edcfccf213d
niekwit <41302859+niekwit@users.noreply.github.com>
2026-04-22 09:27:47 +0100

    fixed log sink

--- workflow/scripts/te_pos_coverage.R
+++ workflow/scripts/te_pos_coverage.R
@@ -1,7 +1,7 @@
 # Redirect R output to log
 log <- file(snakemake@log[[1]], open = "wt")
 sink(log, type = "output")
-sink(log, type = "print")
+sink(log, type = "message")
 
 library(Rsamtools)
 library(Biostrings)
