eb9b4ce18c3781acae8b03807b2b11d782776f91
niekwit <41302859+niekwit@users.noreply.github.com>
2026-06-11 14:01:32 +0100

    plotting colours consistent with other scripts

--- workflow/scripts/te_small_top_families.R
+++ workflow/scripts/te_small_top_families.R
@@ -115,12 +115,12 @@ df_filtered <- df_filtered %>%
   left_join(order_df, by = c("Class", "Subfamily")) %>%
   mutate(Subfamily = reorder_within(Subfamily, -order_val, Class))
 
-if (length(condition_levels) == 2) {
-  colours <- setNames(c("#cccccc", "#dd3b3b"), condition_levels)
+if (length(conditions) == 2) {
+  colours <- c("#cccccc", "#dd3b3b")
 } else {
-  colours <- setNames(
-    RColorBrewer::brewer.pal(n = length(condition_levels), name = "Set3"),
-    condition_levels
+  colours <- c(
+    "#cccccc",
+    RColorBrewer::brewer.pal(n = length(conditions) - 1, name = "Set1")
   )
 }
 
